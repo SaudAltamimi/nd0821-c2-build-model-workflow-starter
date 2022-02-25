9dd94eb772845a2d7b44966d6f89e813cbec8c31
Saud Altamimi <47983804+SaudAltamimi@users.noreply.github.com>
2022-02-25 20:34:29 +0300

    add proper geolocation cleaning step

--- src/basic_cleaning/run.py
+++ src/basic_cleaning/run.py
@@ -34,6 +34,10 @@ def go(args):
     # Convert last_review to datetime
     df['last_review'] = pd.to_datetime(df['last_review'])
 
+    # drop rows in the dataset that are not in the proper geolocation
+    idx = df['longitude'].between(-74.25, -73.50) & df['latitude'].between(40.5, 41.2)
+    df = df[idx].copy()
+
     logger.info(f"Save the results to a CSV file: clean_sample.csv")
     df.to_csv("clean_sample.csv", index=False)
 
